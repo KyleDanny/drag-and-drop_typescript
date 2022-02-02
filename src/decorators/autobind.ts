83b0eb7a300224989b787b0e7dba9fb5c7cf3c50
Kyle Danny <codingkyle92@gmail.com>
2022-02-02 11:27:09 +0100

    modules using es2015

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,18 +1,16 @@
-namespace App {
-  // autobund decorator
-  export function autobind(
-    _target: any, 
-    _methodName: string, 
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      }
+// autobund decorator
+export function autobind(
+  _target: any, 
+  _methodName: string, 
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
     }
-    return adjDescriptor;
   }
+  return adjDescriptor;
 }
